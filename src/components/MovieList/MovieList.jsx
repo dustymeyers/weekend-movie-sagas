f446e9c19543d4fa50dc161b554d21622e3a5031
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-03-06 10:08:30 -0600

    initial setup for handlePosterClick

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -11,6 +11,11 @@ function MovieList() {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
 
+    const handlePosterClick = (movieId) => {
+      console.log('poster clicked', movieId);
+
+    }
+
     return (
         <main>
             <h1>MovieList</h1>
@@ -19,7 +24,7 @@ function MovieList() {
                     return (
                         <div key={movie.id} >
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                            <img src={movie.poster} alt={movie.title} onClick={() => handlePosterClick(movie.id)}/>
                         </div>
                     );
                 })}
