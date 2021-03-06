6a281fac06d143dbcb111084073c7c8ff0699e17
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-03-06 15:14:32 -0600

    initial setup for AddMovie component

---
+++ src/components/AddMovie/AddMovie.jsx
@@ -0,0 +1,49 @@
+function AddMovie() {
+  return(
+    <>
+      <h2>Add a Movie to the List!</h2>
+      <form>
+        <label>
+          Add the Movie's Title:
+          <input type="text" placeholder="Title of Movie" />
+        </label>
+
+        <label>
+          Upload the Movie's Poster:
+          <input type="text" placeholder="Image Address" />
+        </label>
+        
+        <label>
+          Add the Movie's Genre:
+          <select>
+            <option>Pick One</option>
+            <option>Adventure</option>
+            <option>Animated</option>
+            <option>Biographical</option>
+            <option>Comedy</option>
+            <option>Disaster</option>
+            <option>Drama</option>
+            <option>Epic</option>
+            <option>Fantasy</option>
+            <option>Musical</option>
+            <option>Romantic</option>
+            <option>Science Fiction</option>
+            <option>Space-Opera</option>
+            <option>Superhero</option>
+          </select>
+        </label>
+
+        <br/>
+        <label>
+          Add the Movie's Description:
+          <textarea placeholder="Movie Description" rows="4" cols ="50"></textarea>
+        </label>
+
+        <button>Cancel</button>
+        <button>Save</button>
+      </form>
+    </>
+  );
+} // end AddMovie
+
+export default AddMovie;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,11 @@ import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
 // Components
-import MovieList from '../MovieList/MovieList';
+import AddMovie from '../AddMovie/AddMovie';
 import MovieDetails from '../MovieDetails/MovieDetails';
+import MovieList from '../MovieList/MovieList';
+
+
 
 function App() {
   return (
@@ -15,11 +18,14 @@ function App() {
         </Route>
         
         {/* Details page */}
-        <Route path="/details" >
+        <Route path="/details">
           <MovieDetails />
         </Route>
 
         {/* Add Movie page */}
+        <Route path="/addMovie">
+          <AddMovie />
+        </Route>
       </Router>
     </div>
   );
